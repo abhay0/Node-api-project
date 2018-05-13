5c0b063ea39e2872aa2a23196d749364754774e3
Abhay Pratap Singh <abhaypratap.s@HCL.COM>
2018-05-13 13:49:24 +0530

    Added delete file

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,20 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/ToDoApp', (err, client) => {
+    if(err){
+        return console.log(`unable to connect the Mongo DB : ${err}`)
+    }
+    // console.log("Connected to MongoDB server");
+    const db = client.db('ToDoApp');
+
+    //delete Many
+    db.collection('ToDo').deleteMany({text: 'Walk the dog'}).then(
+        (result) => {
+              console.log(`result: ${result}`)
+        },
+        (err) => {
+            console.log(`Error: ${err}`)
+        }
+    )
+    // client.close();
+});
